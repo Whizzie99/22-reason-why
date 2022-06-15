6ae85fc25555449ffcfd287349713eb5a76d6046
wisdom obioma <wisdomobiomadev@gmail.com>
2022-06-15 10:57:13 +0100

    add data and animations

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import { StyledFooter } from './styles';
 const Footer = () => {
 	return (
 		<StyledFooter>
-			<p data-aos="fade-up" data-aos-duration="1300">
+			<p data-aos="fade-up" data-aos-duration="1200">
 				specially crafted for Candy!❤️
 			</p>
 		</StyledFooter>
